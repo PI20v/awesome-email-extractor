a33fe8c83fb8ecc9b5707d2d9c1fb40e81f9f877
Maxim Slipenko <maxim@slipenko.com>
2022-05-14 10:11:05 +0300

    меняет название с Form1 на MainForm

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AwesomeEmailExtractor
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Главная форма";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
