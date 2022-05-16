bc0d2385f7a3c8333ddc06aad7e677696f1408bd
Maxim Slipenko <maxim@slipenko.com>
2022-05-16 15:50:40 +0300

    интегрирует алгоритм в обработчик кнопки

--- MainForm.cs
+++ MainForm.cs
@@ -19,6 +19,19 @@ namespace AwesomeEmailExtractor
 
         private void executeButton_Click(object sender, EventArgs e)
         {
+            // Объявляем список уникальных e-mail-ов
+            List<string> uniqueEmails = new List<string>();
+
+            // Получаем исходный текст из sourceRichTextBox
+            string sourceText = sourceRichTextBox.Text;
+
+            // Вызываем метод для извлечения e-mail-ов
+            int count = ExtactEmailsAlgorithm.Extract(sourceText, out uniqueEmails);
+
+            // Выводим результат
+            toolStripStatusLabel.Text = "Успех!";
+            label1.Text = $"Количество e-mail-ов в тексте: {count}";
+            uniqueListBox.DataSource = uniqueEmails;
         }
     }
 }
